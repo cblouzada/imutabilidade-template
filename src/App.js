80a3df6ff8ab1f47067141f650951814f2daa1b5
cblouzada <minhavidaemfloripa@hotmail.com>
2022-10-17 17:29:19 -0300

    Aula de exercicios

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import Garagem from "./Componentes/Garagem";
+import {useState} from "react"
+import Garagem from "./Componentes/GaragemOne/Garagem";
 import { GlobalStyled } from "./globalStyled";
 
+
 export default function App() {
-  const nome = "Turma";
+
+  // const nome = "Turma";
+  const [nome, setNome] = useState("Turma")
+
+
+
   function apresentaGaragem() {
+
     alert(`Boas vindas à garagem de ${nome}`);
+    console.log("nome:".nome)
   }
 
   return (
     <div className="App">
       <GlobalStyled />
 
-      <Garagem nome={nome} mensagemAprentacao={apresentaGaragem} />
+      <Garagem
+      nome={nome} 
+      mensagemAprentacao={apresentaGaragem}
+      setNome={setNome}
+      />
     </div>
   );
 }
